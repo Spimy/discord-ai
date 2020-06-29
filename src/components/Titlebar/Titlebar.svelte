6b47cc75569e2e92be13418a91485df35a7e6cdb
Spimy <officialspyme@gmail.com>
2020-06-29 23:05:10 +0400

    Add titlebar component

---
+++ src/components/Titlebar/Titlebar.svelte
@@ -0,0 +1,36 @@
+<script>
+
+    const { remote } = require('electron');
+    
+    const minimize = () => {
+        remote.getCurrentWindow().minimize();
+    }
+
+    const minMaximize = () => {
+        const currentWindow = remote.getCurrentWindow();
+        if (currentWindow.isMaximized()) {
+            currentWindow.unmaximize();
+        } else {
+            currentWindow.maximize();
+        }
+    }
+
+    const quit = () => {
+        remote.app.quit();
+    }
+
+</script>
+
+<div id="Titlebar">
+
+    <div class="app-name-container">
+        <img class="logo" src="./assets/discord.svg" alt="Discord Logo">
+    </div>
+
+    <div class="window-controls-container">
+        <button id="minimize-button" class="minimize-button" on:click={minimize}>–</button>
+        <button id="min-max-button" class="min-max-button" on:click={minMaximize}>◻</button>
+        <button id="close-button" class="close-button" on:click={quit}>✕</button>
+    </div>
+
+</div>
