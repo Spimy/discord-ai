3dbf56bcfb2d42f383276d4c32e239e54c243968
Eray Chumak <eray_chumak@protonmail.com>
2020-07-01 11:54:00 +0100

    remove Modal for auth

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,14 @@
 <script>
-	import Titlebar from './components/Titlebar/Titlebar.svelte';
-	import Modal from './components/Modal/Modal.svelte';
-
-	let showModal = false;
-
-	const toggleModal = () => {
-		showModal = !showModal;
-	}
-
+	import Titlebar from "./components/Titlebar/Titlebar.svelte";
 </script>
 
 <main>
-
-	<Titlebar />
-	<button style="background-color: #000; border:none; padding: .75em 1em;" on:click={toggleModal}>Open Modal</button>
-	<Modal showModal={showModal} on:click={toggleModal}>
-		<label for="token_input">Enter bot token</label>
-		<input id="token_input" type="text" placeholder="...">
-	</Modal>
+	<Titlebar/>
+	<div class="page home">
+		<div class="field">
+			<p>Enter your bot token:</p>
+			<input/>
+		</div>
+		<button>Auth</button>
+	</div>
 </main>
