929e05ec70cd6fc89c71a71b1bc324c1e92b5d0c
Eray Chumak <eray_chumak@protonmail.com>
2020-07-01 11:56:35 +0100

    fix title bar style

--- src/components/Titlebar/Titlebar.svelte
+++ src/components/Titlebar/Titlebar.svelte
@@ -1,36 +1,44 @@
 <script>
+    const { remote } = require("electron");
 
-    const { remote } = require('electron');
-    
-    const minimize = () => {
-        remote.getCurrentWindow().minimize();
-    }
+    const minimize = () => (
+        remote.getCurrentWindow().minimize()
+    );
 
     const minMaximize = () => {
-        const currentWindow = remote.getCurrentWindow();
-        if (currentWindow.isMaximized()) {
-            currentWindow.unmaximize();
+        const window = remote.getCurrentWindow();
+        let isMaximized = window.isFullScreen();
+
+        if (isMaximized) {
+            window.unmaximize();
         } else {
-            currentWindow.maximize();
+            window.maximize();
         }
-    }
+    };
 
-    const quit = () => {
-        remote.app.quit();
-    }
-
+    const quit = () => remote.app.quit();
 </script>
 
 <div id="Titlebar">
-
-    <div class="app-name-container">
-        <img class="logo" src="./assets/discord.svg" alt="Discord Logo">
+    <div class="logo">
+        <img src="./assets/discord.svg" alt="Discord Logo"/>
+        <p>AI</p>
     </div>
-
-    <div class="window-controls-container">
-        <button id="minimize-button" class="minimize-button" on:click={minimize}>–</button>
-        <button id="min-max-button" class="min-max-button" on:click={minMaximize}>◻</button>
-        <button id="close-button" class="close-button" on:click={quit}>✕</button>
+    <div class="controls">
+        <div class="btn" on:click={minimize}>
+            <svg aria-hidden="false" width="12" height="12" viewBox="0 0 12 12">
+                <rect fill="currentColor" width="10" height="1" x="1" y="6"/>
+            </svg>
+        </div>
+        <div class="btn" on:click={minMaximize}>
+            <svg aria-hidden="false" width="12" height="12" viewBox="0 0 12 12">
+                <rect width="9" height="9" x="1.5" y="1.5" fill="none" stroke="currentColor"/>
+            </svg>
+        </div>
+        <div class="btn close" on:click={quit}>
+            <svg aria-hidden="false" width="12" height="12" viewBox="0 0 12 12">
+                <polygon fill="currentColor" fill-rule="evenodd" points="11 1.576 6.583 6 11 10.424 10.424 11 6 6.583 1.576 11 1 10.424 5.417 6 1 1.576 1.576 1 6 5.417 10.424 1"/>
+            </svg>
+        </div>
     </div>
-
 </div>
